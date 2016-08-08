528a36b4561c659feb0d0d10226a8d22903cfa65
Sébastien <muyangyi@users.noreply.github.com>
2016-08-08 14:52:58 +0800

    Update pid.hpp

--- src/drone_controller/pid.hpp
+++ src/drone_controller/pid.hpp
@@ -5,14 +5,17 @@ class PIDController{
 public:
 	/* coefficients for P, I, D*/
 	float kP, kI, kD;
+	
 	/* coefficient for filtering the derivative values */
 	float kN;
 	float _P, _I, _D;
 	float old_err, cur_err;
 	float old_time;
     	float errs[20];
+    	
 	/* error integration*/
 	float cur_I;
+	
 	/* error derivative*/
 	float old_D;
 	PIDController():kP(0.0),kI(0.0),kD(0.0){reset();}
